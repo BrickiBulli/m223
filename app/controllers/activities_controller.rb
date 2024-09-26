4d06f4285f4a4abcc0e3f773c423329d730143aa
BrickiBulli <seth.schmutz@bbzbl-it.ch>
2024-09-26 16:16:00 +0200

    Other changes idk

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,16 @@
 class ActivitiesController < ApplicationController
+  before_action :require_login
+  before_action :check_storage_manager, only: [:index]
+
   def index
     @versions = PaperTrail::Version.all.order(created_at: :desc)  # Get all versions ordered by created_at
+  end
+
+  private
+
+  def check_storage_manager
+    unless @current_employee&.storage_manager?
+      redirect_to root_path, alert: "You are not authorized to create an employee."
+    end
   end
 end
